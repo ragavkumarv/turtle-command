d27e79df184ddf3842c70bfecfbf450cc3efc965
ragavkumarv <ragavkumarv@gmail.com>
2017-07-05 20:23:57 +0530

    Obstacles with ramda unique obstacles

--- src/app/turtle-grid/obstacle.ts
+++ src/app/turtle-grid/obstacle.ts
@@ -1,4 +1,5 @@
 import {forwards} from './movement';
+import {contains} from 'ramda';
 export const boundaryCond = ({gridLength, gridWidth} , currPos) => {
  const  {posX , posY, dir} = forwards(currPos);
   return (posX > 1 && posY > 1 && posX < gridLength && posY < gridWidth);
@@ -7,18 +8,23 @@ const randNum = (max, min) => Math.floor(Math.random() * (max - min + 1) + min);
 // const randPair = (gL,gW) => ({X:randNum(gL,1),Y:randNum(gW,1)})
 const randPair = (gL, gW) => ({
   X: randNum(gL, 1),
-  get Y() {
-    return this.X === gL ? randNum(gW, 1) : gL;
-  }
+  Y: randNum(gW, 1)
 });
 
+const randPairs = (gL, gW) => {
+  if (gW === 1 || gL === 1) {
+    return [randPair(gL, gW)];
+  }
+  const accPairs = randPairs(gL - 1, gW - 1);
+  let newPair = randPair(gL, gW);
+  while (contains(newPair, accPairs)) {
+    newPair = randPair(gL, gW);
+  }
+  return [newPair].concat(accPairs);
+};
 
-const randPairs = (gL, gW) =>
-  gW === 1 || gL === 1
-    ? [randPair(gL, gW)]
-    : [randPair(gL, gW)].concat(randPairs(gL - 1, gW - 1));
-
-console.log(randPairs(7, 7));
+// const InitPosCheck = (InitPos,randObsPairs) => R.contains(InitPos,)
+console.log(randPairs(9, 9));
 
 export const obstacles = ({gridLength, gridWidth}, {posX , posY, dir}) => {
 
